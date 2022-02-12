ca3c7d5caa0681a0f4d04087e8288189c086372c
Rodrigo Catalán López <shl@MacBook-Air-de-Rodrigo.local>
2022-02-12 01:58:02 -0300

    api ok, funcionando get, next y prev

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,23 +1,44 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { getPokemonAction } from '../redux/pokeDuck';
+import { getPokemonAction, nextPokemonAction, prevPokemonAction } from '../redux/pokeDuck';
 
 const Pokemon = () => {
     const dispatch = useDispatch();
-    const pokemons = useSelector(store => store.pokemons.array)
-    console.log(pokemons)
+    const pokemons = useSelector(store => store.pokemons.results)
+    const next = useSelector(store => store.pokemons.next)
+    const previous = useSelector(store => store.pokemons.previous)
+
     return (
     <div>
         <h2>Pokemon's List</h2><hr />
-        <button className='btn btn-primary ' onClick={() => dispatch(getPokemonAction())}>
-            GetPoke
-        </button>
+
+        {
+            pokemons.length === 0 &&
+             <button className='btn btn-primary' onClick={() => dispatch(getPokemonAction())}>
+                GetPoke
+            </button>
+        }
+        {
+            next && 
+            <button className='btn btn-secondary' onClick={() => dispatch(nextPokemonAction())}>
+                NextPoke
+            </button>
+        }
+        {
+            previous && 
+            <button className='btn btn-warning' onClick={() => dispatch(prevPokemonAction())}>
+                PrevPoke
+            </button>
+        }
+       
+        
+        
         <hr />
         <ul>
-        {
-                pokemons.map(item => (
-                    <li key={item.name}> {item.name}</li>
-                 ))
+            {
+            pokemons.map(item => (
+                <li key={item.name}> {item.name}</li>
+            ))
             }
         </ul>
     </div>
--- src/redux/pokeDuck.js
+++ src/redux/pokeDuck.js
@@ -2,16 +2,22 @@ import axios from "axios";
 
 //const 
 const initialData = {
-    array : []
+    count: 0,
+    next: null,
+    previous: null,
+    results: []
 }
 
 const GET_POKEMON_OK = 'GET_POKEMON_OK';
+const NEXT_POKEMON_OK = 'NEXT_POKEMON_OK';
 
 //reducer 
 export default function pokeReducer(state = initialData, action) {
     switch(action.type){
         case GET_POKEMON_OK:
-            return {...state, array: action.payload}
+            return {...state, ...action.payload}
+        case NEXT_POKEMON_OK:
+            return {...state, ...action.payload}
         default:
             return state
     }
@@ -19,11 +25,40 @@ export default function pokeReducer(state = initialData, action) {
 
 //actions
 export const getPokemonAction = () => async (dispatch, getState) => {
+    
     try {
-        const res = await axios.get('https://pokeapi.co/api/v2/pokemon?offset=0&limit=20');
+        const res = await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=20`);
         dispatch({
             type: GET_POKEMON_OK,
-            payload: res.data.results
+            payload: res.data
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const nextPokemonAction = () => async (dispatch, getState) => {
+    const {next} = getState().pokemons;
+    
+    try {
+        const res = await axios.get(next);
+        dispatch({
+            type: NEXT_POKEMON_OK,
+            payload: res.data
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const prevPokemonAction = () => async (dispatch, getState) => {
+    const {previous} = getState().pokemons;
+    
+    try {
+        const res = await axios.get(previous);
+        dispatch({
+            type: NEXT_POKEMON_OK,
+            payload: res.data
         })
     } catch (error) {
         console.log(error)
